f6cf0b9d2b16e278afc2f468692a05c107100852
María Agustina <agustina.filosofia20@gmail.com>
2026-06-29 14:04:16 -0300

    add django logging

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,33 @@ CORS_ALLOW_ALL_ORIGINS = True
 # =========================
 
 ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API_KEY")
+
+# =========================
+# 📝 LOGGING
+# =========================
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
